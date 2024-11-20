79e1b9b305f025fdedcca545e9607c663ed048b5
KevinWh0 <45321184+ChaoticLeah@users.noreply.github.com>
2024-11-20 18:02:22 +0100

    title

--- ui/src/routes/+layout.svelte
+++ ui/src/routes/+layout.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
 	import '../app.css';
 	let { children } = $props();
+	
+	let title = `Recipe Manager`;
 </script>
+
+<svelte:head>
+    <title>{title}</title>
+</svelte:head>
 
 {@render children()}
